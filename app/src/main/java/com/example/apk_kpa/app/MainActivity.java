10fffd53377e2fd3b98ff55f7c30d5fa3bd68dd1
Dofke <dovix112@gmail.com>
2014-06-01 23:08:48 +0300

    23:08

--- app/src/main/java/com/example/apk_kpa/app/MainActivity.java
+++ app/src/main/java/com/example/apk_kpa/app/MainActivity.java
@@ -12,19 +12,17 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        startActivity(new Intent(this, Login.class));
     }
 
-    //metodas register klases iskvietimui
-    public void register (View view1) {
 
-        startActivity(new Intent(this, Register.class));
-    }
 
-    public void log_in (View view2) {
+    public void loged_in (View view2) {
 
         startActivity(new Intent(this, LogedIn.class));
     }
+
+
 //    public void test(View view){
 //        startActivity(new Intent(this, Admin_loged.class));
 //    }
