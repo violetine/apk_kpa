e2c50b1e6451312ed1f84061adf39bdb05e54286
goodperson99 <tcimermonas@gmail.com>
2014-06-05 13:21:46 +0300

    sukurtas admin sukurti klausima xml'as, taip pat testinis vibratorius

--- app/src/main/java/com/example/apk_kpa/app/Admin.java
+++ app/src/main/java/com/example/apk_kpa/app/Admin.java
@@ -2,15 +2,30 @@ package com.example.apk_kpa.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
 
 /**
  * Created by Dofke on 2014-06-04.
  */
-public class Admin extends Activity {
+public class Admin extends Activity implements View.OnClickListener {
+
+    private View myView;
+    private Vibrator vibr;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loged_in_admin);
+        vibr = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
 
+        myView = this.findViewById(R.id.kurtiApklausaBtn);
+        myView.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v){
+        vibr.vibrate(50);
+    }
+
+
 }
