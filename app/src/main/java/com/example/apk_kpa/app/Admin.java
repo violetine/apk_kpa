968815f8e6ffa2a7ec6ff2339911ce310a833438
Dofke <dovix112@gmail.com>
2014-06-09 00:43:34 +0300

    Sutvarkytas slide Swipe,veikia adminui.

--- app/src/main/java/com/example/apk_kpa/app/Admin.java
+++ app/src/main/java/com/example/apk_kpa/app/Admin.java
@@ -62,18 +62,9 @@ public class Admin extends Activity {
         navDrawerItems = new ArrayList<NavDrawerItem>();
 
         // adding nav drawer items to array
-        // Home
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
-        // Find People
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-        // Photos
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-        // Communities, Will add a counter here
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
-        // Pages
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
-        // What's hot, We  will add a counter here
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1), true, "50+"));
+
+        // Admin control
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1), true, "10+"));
 
 
         // Recycle the typed array
@@ -168,24 +159,8 @@ public class Admin extends Activity {
         Fragment fragment = null;
         switch (position) {
             case 0:
-                fragment = new HomeFragment();
+                fragment = new Admin_loged();
                 break;
-            case 1:
-                fragment = new FindPeopleFragment();
-                break;
-            case 2:
-                fragment = new PhotosFragment();
-                break;
-            case 3:
-                fragment = new CommunityFragment();
-                break;
-            case 4:
-                fragment = new PagesFragment();
-                break;
-            case 5:
-                fragment = new WhatsHotFragment();
-                break;
-
             default:
                 break;
         }
--- app/src/main/java/com/example/apk_kpa/app/Admin_loged.java
+++ app/src/main/java/com/example/apk_kpa/app/Admin_loged.java
@@ -6,16 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
-	
-	public HomeFragment(){}
-	
-	@Override
+public class Admin_loged extends Fragment {
+
+    public Admin_loged(){}
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
+                             Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.loged_in_admin, container, false);
-         
+
         return rootView;
     }
 }
