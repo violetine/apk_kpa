6499893786422b76c80c3ea5ef6984ba8e87aa5a
Dofke <dovix112@gmail.com>
2014-06-18 23:05:30 +0300

    Padarytas fullscreen.

--- app/src/main/java/com/example/apk_kpa/app/LogedIn.java
+++ app/src/main/java/com/example/apk_kpa/app/LogedIn.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 /**
@@ -13,6 +15,11 @@ public class LogedIn extends Activity {
     private String slapyvardis;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.loged_in);
 
         Bundle extras = getIntent().getExtras();
--- app/src/main/java/com/example/apk_kpa/app/Login.java
+++ app/src/main/java/com/example/apk_kpa/app/Login.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -45,7 +47,13 @@ public class Login extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.login);
+
 
         // elementai
         final EditText e_code = (EditText) findViewById(R.id.code);
