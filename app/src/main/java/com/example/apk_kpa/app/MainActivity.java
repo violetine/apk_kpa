944b05c1af1fae03d52596be920ab04b236d3eed
Dofke <dovix112@gmail.com>
2014-06-21 17:04:05 +0300

    mainActivity

--- app/src/main/java/com/example/apk_kpa/app/MainActivity.java
+++ app/src/main/java/com/example/apk_kpa/app/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.apk_kpa.app;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -13,6 +17,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        startActivity(new Intent(this, Login.class));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
@@ -21,6 +28,10 @@ public class MainActivity extends Activity {
     public void loged_in (View view2) {
 
         startActivity(new Intent(this, LogedIn.class));
+    }
+
+    public void logIn (View view3){
+        startActivity(new Intent(this, Login.class));
     }
 
 
